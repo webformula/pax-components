14af1704118178b161f9e7cbb7bfef34b7e32173
Ben Rubin <ben@benjaminrubin.com>
2019-07-24 22:50:55 -0500

    add href attribute to list-item add right and left border to drawer

--- src/components/list-item/index.js
+++ src/components/list-item/index.js
@@ -1,18 +1,35 @@
 customElements.define('mdw-list-item', class extends HTMLElementExtended {
   constructor() {
     super();
+    this.bound_hrefClick = this.hrefClick.bind(this);
   }
 
   connectedCallback() {
+    this.connectRipple();
+    this.connectHREF();
+  }
+
+  disconnectedCallback() {
+    if (this.ripple) this.ripple.destroy();
+    this.removeEventListener('click', this.bound_hrefClick);
+  }
+
+  connectRipple() {
     const element = this.querySelector('.mdw-ripple');
     if (!element) return;
     this.ripple = new MDWRipple({
       element,
       triggerElement: this
     });
+    this.classList.add('mdw-has-ripple');
   }
 
-  disconnectedCallback() {
-    if (this.ripple) this.ripple.destroy();
+  connectHREF() {
+    if (!this.hasAttribute('href')) return;
+    this.addEventListener('click', this.bound_hrefClick);
+  }
+
+  hrefClick() {
+    document.location.href = this.getAttribute('href');
   }
 });
