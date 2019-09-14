83d2b8e0ff830dcc21118fa92483428f41f53e74
Ben Rubin <ben@benjaminrubin.com>
2019-09-14 10:28:34 -0500

    finish up v1

--- src-docs/pages/components/bottom-navigation.js
+++ src-docs/pages/components/bottom-navigation.js
@@ -18,17 +18,76 @@ export default class BottomNavigation extends Page {
           <mdw-bottom-navigation>
             <mdw-button>
               <mdw-icon>home</mdw-icon>
-              Home
             </mdw-button>
 
             <mdw-button>
-              <mdw-icon>home</mdw-icon>
+              <mdw-icon>start</mdw-icon>
+              Favs
+            </mdw-button>
+
+            <mdw-button>
+              <mdw-icon>note</mdw-icon>
+              Notes
+            </mdw-button>
+
+            <mdw-button>
+              <mdw-icon>cake</mdw-icon>
+              BDay
             </mdw-button>
           </mdw-bottom-navigation>
         </div>
 
-        <a hreef="https://material.io/design/components/app-bars-top.html#">Material Design Guidlines: Top app bar</a>
-        <p>The top app bar displays information and actions relating to the current screen</p>
+        <a hreef="https://material.io/components/bottom-navigation/">Material Design Guidlines: Bottom navigation</a>
+        <p>Bottom navigation bars allow movement between primary destinations in an app</p>
+
+
+        <section id="types">
+          <h4>Example: mobile</h4>
+
+          <div class="codecase">
+            <div class="title">Mobile</div>
+            <code-mirror mode="html">
+                <mdw-bottom-navigation>
+                  <mdw-button href="#/">
+                    <mdw-icon>home</mdw-icon>
+                    Home
+                  </mdw-button>
+
+                  <mdw-button href="#/">
+                    <mdw-icon>start</mdw-icon>
+                    Favs
+                  </mdw-button>
+
+                  <mdw-button href="#/">
+                    <mdw-icon>note</mdw-icon>
+                    Notes
+                  </mdw-button>
+                </mdw-bottom-navigation>
+            </code-mirror>
+            <div class="demo">
+              <iframe style="width: 400px; height: 600px; border: 1px solid #ddd" src="bottom-navigation-mobile.html"></iframe>
+            </div>
+          </div>
+
+          <!-- <div class="codecase">
+            <div class="title">Fixed</div>
+            <code-mirror mode="html">
+              <mdw-top-app-bar class="mdw-fixed">
+                <section>
+                  <mdw-icon>menu</mdw-icon>
+                  <span class="title">Fixed</span>
+                </section>
+                <section>
+                  <mdw-icon>bookmark</mdw-icon>
+                </section>
+              </mdw-top-app-bar>
+            </code-mirror>
+            <div class="demo">
+              <iframe style="width: 100%; height: 200px; border: 1px solid #ddd" src="top-app-bar-fixed.html"></iframe>
+            </div>
+          </div> -->
+
+        </section>
 
       </article>
     `;
