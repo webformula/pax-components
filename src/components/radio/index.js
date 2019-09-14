bdd3f3af1474cfb6f28282c224a8e77f37bfec6c
Ben Rubin <ben@benjaminrubin.com>
2019-09-14 11:40:18 -0500

    add horizontal with labels update html structure

--- src/components/radio/index.js
+++ src/components/radio/index.js
@@ -1,14 +1,18 @@
 import { HTMLElementExtended } from '@webformula/pax-core';
 import MDWRipple from '../../core/Ripple.js';
+import MDWUtils from '../../core/Utils.js';
 
 customElements.define('mdw-radio', class extends HTMLElementExtended {
   constructor() {
     super();
     // input radio will not work correctly in shadowroot
-    this.insertAdjacentHTML('afterbegin', this.template());
+    this.insertAdjacentHTML('beforeend', this.rippleTemplate());
   }
 
   connectedCallback() {
+    if (this.parentNode.initialValue === this.value) this.input.checked = true;
+    if (!this.input.hasAttribute('type')) this.input.setAttribute('type', 'radio');
+    if (!this.input.hasAttribute('name')) this.input.setAttribute('name', this.name);
     this.ripple = new MDWRipple({
       element: this.querySelector('.mdw-ripple'),
       triggerElement: [this.input],
@@ -21,6 +25,10 @@ customElements.define('mdw-radio', class extends HTMLElementExtended {
     this.ripple.destroy();
   }
 
+  get value() {
+    return this.input.value;
+  }
+
   get input() {
     return this.querySelector('input');
   }
@@ -31,12 +39,19 @@ customElements.define('mdw-radio', class extends HTMLElementExtended {
     } else if (this.hasAttribute('name')) {
       this.name_ = this.getAttribute('name');
     }
-    return this.name_ || '';
+
+    // create name if one was not provided
+    // name is required for radio buttons to work
+    if (!this.name_) {
+      this.name_ = MDWUtils.uid();
+      if (this.parentNode) this.parentNode.setAttribute('name', this.name_);
+      else this.setAttribute('name', this.name_);
+    }
+    return this.name_;
   }
 
-  template() {
+  rippleTemplate() {
     return `
-      <input type="radio" name="${this.name}">
       <div class="mdw-radio-background">
         <div class="mdw-radio__outer-circle"></div>
         <div class="mdw-radio__inner-circle"></div>
