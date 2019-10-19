4e4f5f02ec467f739b5e9fc6108dbd28d3097bf6
Ben Rubin <ben@benjaminrubin.com>
2019-10-19 10:50:29 -0500

    up

--- src-docs/layout/index.js
+++ src-docs/layout/index.js
@@ -9,6 +9,7 @@ export default function ({ head, body, title }) {
       <head>
         <title>${title}</title>
 
+        <meta http-equiv="Cache-Control" content="no-store" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
