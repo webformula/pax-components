ed468204316a80a5e87a0ad21bfb93a69888cdc9
Ben Rubin <ben@benjaminrubin.com>
2020-06-05 18:21:39 -0400

    put fix in for bup component registration

--- src-docs/components/anchor-link.js
+++ src-docs/components/anchor-link.js
@@ -1,69 +1,73 @@
 import { HTMLElementExtended, router } from '@webformula/pax-core';
 
-customElements.define('anchor-link', class extends HTMLElementExtended {
-  constructor() {
-    super();
-    this.addEventListener('click', this.scrollTo.bind(this));
-  }
-
-  connectedCallback() {
-    const params = router.searchParamters;
-    if (params && params.anchor && params.anchor.replace('hash-', '#') === this.selector) {
-      setTimeout(() => {
-        this.moveTo();
-      }, 0);
+// this is called twice because the docs loads multiple versions of the code. To prevent an error we are double checking for the existence
+if (!customElements.get('anchor-link')) {
+  customElements.define('anchor-link', class extends HTMLElementExtended {
+    constructor() {
+      super();
+      this.addEventListener('click', this.scrollTo.bind(this));
     }
-  }
 
-  scrollTo() {
-    let anchor = this.getAnchor();
-    let scrollElement = this.getScrollElement();
-    let count = anchor.offsetTop - scrollElement.scrollTop - this.offset;
-    scrollElement.scrollBy({
-      top: count,
-      left: 0,
-      behavior: 'smooth'
-    });
-    router.setSearchParamter('anchor', this.selector.replace('#', 'hash-'));
-  }
-
-  moveTo() {
-    let anchor = this.getAnchor();
-    let scrollElement = this.getScrollElement();
-    let count = anchor.offsetTop - scrollElement.scrollTop - this.offset;
-    scrollElement.scrollTop = count;
-    router.setSearchParamter('anchor', this.selector.replace('#', 'hash-'));
-  }
-
-  get selector() {
-    return this.getAttribute('selector');
-  }
-
-  get offset() {
-    return (this.getAttribute('offset') || '0').replace('px', '');
-  }
-
-  getAnchor() {
-    let p = this;
-    while (p = p.parentNode) {
-      if (p.nodeType === 11 || p.nodeType === 9) {
-        return p.querySelector(this.selector);
+    connectedCallback() {
+      const params = router.searchParamters;
+      if (params && params.anchor && params.anchor.replace('hash-', '#') === this.selector) {
+        setTimeout(() => {
+          this.moveTo();
+        }, 0);
       }
     }
-  }
 
-  getScrollElement() {
-    let parent = this.parentNode;
-    while (parent !== undefined && this.hasComputedStyleValue('overflowY', parent) === false) {
-      if (parent.nodeType === 9) parent = undefined;
-      else parent = parent.parentNode || parent.host;
+    scrollTo() {
+      let anchor = this.getAnchor();
+      let scrollElement = this.getScrollElement();
+      let count = anchor.offsetTop - scrollElement.scrollTop - this.offset;
+      scrollElement.scrollBy({
+        top: count,
+        left: 0,
+        behavior: 'smooth'
+      });
+      router.setSearchParamter('anchor', this.selector.replace('#', 'hash-'));
     }
-    return parent;
-  }
 
-  hasComputedStyleValue(key, target) {
-    if (!target || target.nodeType !== 1) return false;
-    let computedStyles = window.getComputedStyle(target);
-    return typeof computedStyles[key] !== 'undefined' && (computedStyles[key] == 'scroll' || computedStyles[key] == 'auto');
-  }
-});
+    moveTo() {
+      let anchor = this.getAnchor();
+      let scrollElement = this.getScrollElement();
+      let count = anchor.offsetTop - scrollElement.scrollTop - this.offset;
+      scrollElement.scrollTop = count;
+      router.setSearchParamter('anchor', this.selector.replace('#', 'hash-'));
+    }
+
+    get selector() {
+      return this.getAttribute('selector');
+    }
+
+    get offset() {
+      return (this.getAttribute('offset') || '0').replace('px', '');
+    }
+
+    getAnchor() {
+      let p = this;
+      while (p = p.parentNode) {
+        if (p.nodeType === 11 || p.nodeType === 9) {
+          return p.querySelector(this.selector);
+        }
+      }
+    }
+
+    getScrollElement() {
+      let parent = this.parentNode;
+      while (parent !== undefined && this.hasComputedStyleValue('overflowY', parent) === false) {
+        if (parent.nodeType === 9) parent = undefined;
+        else parent = parent.parentNode || parent.host;
+      }
+      return parent;
+    }
+
+    hasComputedStyleValue(key, target) {
+      if (!target || target.nodeType !== 1) return false;
+      let computedStyles = window.getComputedStyle(target);
+      return typeof computedStyles[key] !== 'undefined' && (computedStyles[key] == 'scroll' || computedStyles[key] == 'auto');
+    }
+  });
+
+}
--- src-docs/components/monaco-editor.js
+++ src-docs/components/monaco-editor.js
@@ -1,139 +1,142 @@
 import { HTMLElementExtended } from '@webformula/pax-core';
 
-customElements.define('monaco-editor', class extends HTMLElementExtended {
-  constructor() {
-    super();
-    this.getContent();
-  }
+// this is called twice because the docs loads multiple versions of the code. To prevent an error we are double checking for the existence
+if (!customElements.get('monaco-editor')) {
+  customElements.define('monaco-editor', class extends HTMLElementExtended {
+    constructor() {
+      super();
+      this.getContent();
+    }
 
-  connectedCallback() {
-    if (window.monacoLoaded) this.init();
-    else this.wait();
-  }
-
-  set content(str = '') {
-    const lines = this.removeLeadingTabs(str.split(/(?:\r\n|\n|\r)/));
-    this.lineCount = lines.length;
-    this._content = lines.join('\n');
-    this.style.height = `${(this.lineCount * 18) + 12}px`;
-    if (window.monacoLoaded) {
-      this.editor.setValue(this._content);
-    } else this.wait();
-  }
-
-  wait() {
-    setTimeout(() => {
+    connectedCallback() {
       if (window.monacoLoaded) this.init();
       else this.wait();
-    }, 500);
-  }
-
-  init() {
-    this.style.height = `${(this.lineCount * 18) + 12}px`;
-    if (this.editor) {
-      this.editor.value = this._content;
-      return;
     }
 
-    this.editor = monaco.editor.create(this, {
-      value: this._content,
-      language: this.language,
-      scrollBeyondLastLine: false,
-      theme: 'NightOwl',
-      minimap: {
-        enabled: false
-      },
-      readOnly: true,
-      autoIndent: true,
-      indentGuides: {
-        enabled: false
-      },
-      automaticLayout: true
-    });
-
-    // prevent keyboard from opening on mobile
-    [...this.querySelectorAll('textarea')].forEach(el => {
-      el.setAttribute('readonly', 'readonly');
-    });
-  }
-
-  singleLineString(strings, ...values) {
-    // Interweave the strings with the
-    // substitution vars first.
-    let output = '';
-    for (let i = 0; i < values.length; i++) {
-      output += strings[i] + values[i];
+    set content(str = '') {
+      const lines = this.removeLeadingTabs(str.split(/(?:\r\n|\n|\r)/));
+      this.lineCount = lines.length;
+      this._content = lines.join('\n');
+      this.style.height = `${(this.lineCount * 18) + 12}px`;
+      if (window.monacoLoaded) {
+        this.editor.setValue(this._content);
+      } else this.wait();
     }
-    output += strings[values.length];
 
-    // Split on newlines.
-    let lines = output.split(/(?:\r\n|\n|\r)/);
+    wait() {
+      setTimeout(() => {
+        if (window.monacoLoaded) this.init();
+        else this.wait();
+      }, 500);
+    }
 
-    // Rip out the leading whitespace.
-    return lines.map((line, i) => {
-      let notSpace = true;
-      if (i === 0) {
-        const spaceCount = line.split(' ').filter(i => {
-          if (i !== ' ') notSpace = false;
-          return notSpace;
-        }).length;
-        console.log('spaceCount', spaceCount);
-      };
-      return line.replace(/^\s+/gm, '');
-    }).join(' ').trim();
-  }
+    init() {
+      this.style.height = `${(this.lineCount * 18) + 12}px`;
+      if (this.editor) {
+        this.editor.value = this._content;
+        return;
+      }
 
-  get language() {
-    let mode = this.hasAttribute('language') ? this.getAttribute('language') : 'javascript';
-    return mode;
-  }
+      this.editor = monaco.editor.create(this, {
+        value: this._content,
+        language: this.language,
+        scrollBeyondLastLine: false,
+        theme: 'NightOwl',
+        minimap: {
+          enabled: false
+        },
+        readOnly: true,
+        autoIndent: true,
+        indentGuides: {
+          enabled: false
+        },
+        automaticLayout: true
+      });
 
-  get code() {
-    if (!this._code) this._code = this.querySelector('code');
-    return this._code;
-  }
+      // prevent keyboard from opening on mobile
+      [...this.querySelectorAll('textarea')].forEach(el => {
+        el.setAttribute('readonly', 'readonly');
+      });
+    }
 
-  removeLeadingTabs(lines) {
-    // remove first line if its empty
-    // this happens because of the html tabbing
-    if (lines[0] === '') lines.shift();
+    singleLineString(strings, ...values) {
+      // Interweave the strings with the
+      // substitution vars first.
+      let output = '';
+      for (let i = 0; i < values.length; i++) {
+        output += strings[i] + values[i];
+      }
+      output += strings[values.length];
 
-    // get number of leading spaces from first line
-    let idSpace = true;
-    const maxSpaceCount = (lines[0] || '1').split(/(?:\s)/).filter(i => {
-      if (i !== '') idSpace = false;
-      return idSpace;
-    }).length - 1;
+      // Split on newlines.
+      let lines = output.split(/(?:\r\n|\n|\r)/);
+
+      // Rip out the leading whitespace.
+      return lines.map((line, i) => {
+        let notSpace = true;
+        if (i === 0) {
+          const spaceCount = line.split(' ').filter(i => {
+            if (i !== ' ') notSpace = false;
+            return notSpace;
+          }).length;
+          console.log('spaceCount', spaceCount);
+        };
+        return line.replace(/^\s+/gm, '');
+      }).join(' ').trim();
+    }
+
+    get language() {
+      let mode = this.hasAttribute('language') ? this.getAttribute('language') : 'javascript';
+      return mode;
+    }
+
+    get code() {
+      if (!this._code) this._code = this.querySelector('code');
+      return this._code;
+    }
+
+    removeLeadingTabs(lines) {
+      // remove first line if its empty
+      // this happens because of the html tabbing
+      if (lines[0] === '') lines.shift();
+
+      // get number of leading spaces from first line
+      let idSpace = true;
+      const maxSpaceCount = (lines[0] || '1').split(/(?:\s)/).filter(i => {
+        if (i !== '') idSpace = false;
+        return idSpace;
+      }).length - 1;
 
 
-    // remove the same amount of spaces form all the lines
-    lines = lines.map(line => {
-      let notSpace = false;
-      return line.split(/(?:\s)/).filter((c, i) => {
-        if (notSpace || i >= maxSpaceCount) return true;
-        if (c !== '') {
-          notSpace = true;
-          return true;
-        }
-        return false;
-      }).join(' ');
-    });
+      // remove the same amount of spaces form all the lines
+      lines = lines.map(line => {
+        let notSpace = false;
+        return line.split(/(?:\s)/).filter((c, i) => {
+          if (notSpace || i >= maxSpaceCount) return true;
+          if (c !== '') {
+            notSpace = true;
+            return true;
+          }
+          return false;
+        }).join(' ');
+      });
 
-    // remove trailing empty line
-    if (lines[lines.length - 1] === '') lines.pop();
+      // remove trailing empty line
+      if (lines[lines.length - 1] === '') lines.pop();
 
-    return lines;
-  }
+      return lines;
+    }
 
-  getContent() {
-    let lines;
-    if (this.hasAttribute('content')) lines = this.getAttribute('content');
-    else lines = this.querySelector('code') ? this.querySelector('code').innerHTML : (this.innerHTML || '');
+    getContent() {
+      let lines;
+      if (this.hasAttribute('content')) lines = this.getAttribute('content');
+      else lines = this.querySelector('code') ? this.querySelector('code').innerHTML : (this.innerHTML || '');
 
-    lines = this.removeLeadingTabs(lines.split(/(?:\r\n|\n|\r)/));
+      lines = this.removeLeadingTabs(lines.split(/(?:\r\n|\n|\r)/));
 
-    this.lineCount = lines.length;
-    this._content = lines.join('\n');
-    this.innerHTML = '';
-  }
-});
+      this.lineCount = lines.length;
+      this._content = lines.join('\n');
+      this.innerHTML = '';
+    }
+  });
+}
