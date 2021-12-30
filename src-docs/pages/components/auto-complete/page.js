cbfd688e8f7f2cbf396b3d12f7a369e0bb85b4c2
Ben Rubin <B-3PO@users.noreply.github.com>
2021-12-29 19:05:00 -0600

    Auto complete (#118)
    * up
    
    * up
    
    * up

---
+++ src-docs/pages/components/auto-complete/page.js
@@ -0,0 +1,105 @@
+import { Page } from '@webformula/pax-core';
+
+export default class AutoComplete extends Page {
+  constructor() {
+    super();
+
+    this.debounced_onInputShow = MDWUtils.debounce(this.onInputShow.bind(this), 300);
+    this.debounced_onInputBasic = MDWUtils.debounce(this.onInputBasic.bind(this), 300);
+    this.debounced_onInputSuggestions = MDWUtils.debounce(this.onInputSuggestions.bind(this), 300);
+    this.debounced_onInputTemplate = MDWUtils.debounce(this.onInputTemplate.bind(this), 300);
+
+    this.values = [
+      { text: 'one' },
+      { text: 'two' },
+      { text: 'three' },
+      { text: 'four' },
+      { text: 'five' }
+    ];
+
+    this.templateItems = [
+      {
+        text: 'one',
+        secondary: 'other one text'
+      },
+      {
+        text: 'two',
+        secondary: 'other two text'
+      },
+      {
+        text: 'three',
+        secondary: 'other three text'
+      },
+      {
+        text: 'four',
+        secondary: 'other four text'
+      },
+      {
+        text: 'five',
+        secondary: 'other five text'
+      }
+    ];
+  }
+
+  get title() {
+    return 'Auto complete';
+  }
+
+  template() {
+    return 'pages/components/auto-complete/page.html';
+  }
+
+  connectedCallback() {
+    document.querySelector('#show-textfield').addEventListener('input', this.debounced_onInputShow);
+    document.querySelector('#basic-textfield').addEventListener('input', this.debounced_onInputBasic);
+    document.querySelector('#suggestion-textfield').addEventListener('input', this.debounced_onInputSuggestions);
+    document.querySelector('#template-textfield').addEventListener('input', this.debounced_onInputTemplate);
+
+    document.querySelector('#template-autocomplete').rowTemplate = data => /*html*/`
+      <mdw-list-item style="padding: 6px 12px;">
+        <div class="mdw-list-item__text">
+          <div class="mdw-list-item__primary-text">
+            ${data.text}
+          </div>
+          <div class="mdw-list-item__secondary-text">
+            ${data.secondary}
+          </div>
+        </div>
+      </mdw-list-item>
+    `;
+  }
+
+  async onInputShow(event) {
+    document.querySelector('#show-auto').loading();
+    await this.wait();
+    document.querySelector('#show-auto').data = this.values.filter(v => v.text.includes(event.target.value));
+  }
+
+  async onInputBasic(event) {
+    document.querySelector('#basic-auto').loading();
+    await this.wait();
+    document.querySelector('#basic-auto').data = this.values.filter(v => v.text.includes(event.target.value));
+  }
+
+  async onInputSuggestions(event) {
+    document.querySelector('#suggestion-autocomplete').loading();
+    await this.wait();
+    document.querySelector('#suggestion-autocomplete').data = this.values.filter(v => v.text.includes(event.target.value));
+  }
+
+  async onInputTemplate(event) {
+    document.querySelector('#template-autocomplete').loading();
+    await this.wait();
+    document.querySelector('#template-autocomplete').data = this.templateItems.filter(v => v.text.includes(event.target.value));
+  }
+
+  onChange(event) {
+    // document.querySelector('#one input').value = 'test';
+  }
+
+  wait(time = 1000) {
+    return new Promise(resolve => {
+      setTimeout(resolve, time);
+    });
+  }
+}
--- src/components/autocomplete/index.js
+++ src/components/autocomplete/index.js
@@ -1,200 +1,314 @@
 import { HTMLElementExtended } from '@webformula/pax-core/index.js';
-import MDWUtils from '../../core/Utils.js';
+import MDWSurface from '../surface/service.js';
 
 customElements.define('mdw-autocomplete', class extends HTMLElementExtended {
   constructor() {
     super();
-    this._hasFilter = this.hasAttribute('filter');
+    
+    this._rowTemplate = this._defaultTemplate.bind(this);
+    this.bound_onBlur = this._onBlur.bind(this);
+    this.bound_onFocus = this._onFocus.bind(this);
+    this.bound_onInput = this._onInput.bind(this);
+    this.bound_onKeydown = this._onKeydown.bind(this);
+    this.bound_onClick = this._onClick.bind(this);
+    this.bound_mouseDown = this._mouseDown.bind(this);
+    this.bound_mouseUp = this._mouseUp.bind(this);
+    this.bound_showSpinner = this._showSpinner.bind(this);
+
+    this.input.setAttribute('autocomplete', 'off');
+  }
+
+  get input() {
+    return this.parentNode.querySelector('input');
+  }
+
+  get data() {
+    return this._data || [];
+  }
+  set data(value = []) {
+    if (!Array.isArray(value)) console.error('mdw-autocomplete.data must br an array');
+    this._data = value;
+    this._updateData();
+  }
+
+  get rowTemplate() {
+    return this._rowTemplate;
+  }
+  set rowTemplate(callback) {
+    if (typeof callback !== 'function') return;
+    this._rowTemplate = callback;
+  }
+
+  get mdwSuggestions() {
+    return this.hasAttribute('mdw-suggestions') && this.getAttribute('mdw-suggestions') !== 'off';
+  }
+  set mdwSuggestions(value = false) {
+    if (value === true || value === 'on') this.setAttribute('mdw-suggestions', 'on');
+    else this.removeAttribute('mdw-suggestions');
+  }
+
+  get _textfieldElement() {
+    return this.parentNode;
+  }
+
+  get _spinnerContainer() {
+    return this._textfieldElement.querySelector('.mdw-spinner-container');
+  }
+
+  get _suggestionHideContainer() {
+    return this._textfieldElement.querySelector('.mdw-autocomplete-suggestion .mdw-suggestion-hide');
+  }
+
+  get _suggestionShowContainer() {
+    return this._textfieldElement.querySelector('.mdw-autocomplete-suggestion .mdw-suggestion-show');
+  }
+
+  static get observedAttributes() {
+    return ['mdw-suggestions'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch (name) {
+      case 'mdw-suggestions':
+        this._isSuggestionsEnabled = this.mdwSuggestions;
+        break;
+    }
   }
 
   connectedCallback() {
-    const target = this.targetInput;
-    target.setAttribute('autocomplete', 'off');
-
-    this._innerHTML = this.innerHTML;
-    this._optionsData = [...this.children].reduce((a, el) => {
-      a[el.innerText] = el.getAttribute('value');
-      return a;
-    }, {});
-    this.innerHTML = '';
-    this.insertAdjacentHTML('beforeend', this.panelHTML);
-    this.panel.innerHTML = this._innerHTML;
-    this.panel.style.minWidth = `${this.targetInput.offsetWidth}px`;
-    this.panel.style.transform = 'scale(1)';
-    this.panel.style.top = `${this.targetInput.offsetHeight + 12}px`;
-    this.panel.ignoreElementOnClickToClose(target);
-
-    this.bound_onTargetFocus = this.onTargetFocus.bind(this);
-    this.bound_onTargetBlur = this.onTargetBlur.bind(this);
-    this.bound_onTargetChange = this.onTargetChange.bind(this);
-    this.bound_onTargetInput = this.onTargetInput.bind(this);
-    this.bound_onPanelClick = this.onPanelClick.bind(this);
-    this.bound_onPanelClose = this.onPanelClose.bind(this);
-    this.bound_onKeyDown = this.onKeyDown.bind(this);
-    this.debounce_filter = MDWUtils.debounce(this.filter.bind(this), 100);
-
-    target.addEventListener('focus', this.bound_onTargetFocus);
-    target.addEventListener('blur', this.bound_onTargetBlur);
-    target.addEventListener('input', this.bound_onTargetInput);
-    this.panel.addEventListener('MDWPanel:closed', this.bound_onPanelClose);
-    document.body.addEventListener('keydown', this.bound_onKeyDown);
+    this.input.addEventListener('focus', this.bound_onFocus);
+    this.input.addEventListener('blur', this.bound_onBlur);
+    this._textfieldElement.insertAdjacentHTML('beforeend', '<div class="mdw-autocomplete-suggestion"><span class="mdw-suggestion-hide"></span><span class="mdw-suggestion-show"></span></div>');
+    this._textfieldElement.insertAdjacentHTML('beforeend', '<span class="mdw-spinner-container"></span>');
   }
 
-  disconnectedCallback() {
-    const target = this.targetInput;
-    if (target) {
-      target.removeEventListener('focus', this.bound_onTargetFocus);
-      target.removeEventListener('blur', this.bound_onTargetBlur);
-      target.removeEventListener('change', this.bound_onTargetChange);
-      target.removeEventListener('input', this.bound_onTargetInput);
-    }
-    this.panel.close();
-    this.panel.removeEventListener('click', this.bound_onPanelClick);
-    this.panel.removeEventListener('MDWPanel:closed', this.bound_onPanelClose);
-    document.body.removeEventListener('keydown', this.bound_onKeyDown);
+  async open() {
+    if (!this._canOpen) return;
+    if (this._isOpen) return;
+    document.body.addEventListener('keydown', this.bound_onKeydown);
+    this._isOpen = true;
+    this._surfaceElement = await MDWSurface.open({
+      mobileComponent: 'panel',
+      desktopComponent: 'panel',
+      scrollPanelWidthPage: true,
+      anchorElement: this.input,
+      autoPosition: true,
+      position: 'center bottom',
+      animation: {
+        type: 'scale',
+        origin: 'top',
+        opacity: true
+      },
+      classes: 'mdw-auto-complete',
+      template: `
+        <mdw-content style="min-width: ${this.input.offsetWidth}px" class="mdw-no-padding">
+          <mdw-list>
+            ${this._data.map((d, i) => this._renderRow(d, i)).join('\n')}
+          </mdw-list>
+        </mdw-content>
+      `
+    });
+
+    this.input.addEventListener('input', this.bound_onInput);
+    this._surfaceElement.element.addEventListener('click', this.bound_onClick);
+    this._surfaceElement.element.addEventListener('mousedown', this.bound_mouseDown);
+    this._surfaceElement.element.addEventListener('mouseup', this.bound_mouseUp);
+
+    this._showSuggestion();
   }
 
-  get targetInput() {
-    return document.body.querySelector(`input[name=${this.getAttribute('for')}]`);
+  close() {
+    if (!this._isOpen) return;
+    this._removeSuggestion();
+    document.body.removeEventListener('keydown', this.bound_onKeydown);
+    this.input.removeEventListener('input', this.bound_onInput);
+    this._surfaceElement.element.removeEventListener('click', this.bound_onClick);
+    this._surfaceElement.element.removeEventListener('mousedown', this.bound_mouseDown);
+    this._surfaceElement.element.removeEventListener('mouseup', this.bound_mouseUp);
+    this._unFocusItem();
+    this._surfaceElement.close();
+    this._surfaceElement = undefined;
+    this._isOpen = false;
   }
 
-  get panel() {
-    if (!this.panel_) this.panel_ = this.querySelector('mdw-panel');
-    return this.panel_;
+  loading() {
+    this.bound_showSpinner();
   }
 
-  get panelHTML() {
-    return '<mdw-panel mdw-position="bottom inner-left"></mdw-panel>';
+
+  // --- private ---
+
+  _updateData() {
+    this._resetFocus();
+    this._hideSpinner();
+    if (this._data.length === 0) return this.close();
+
+    // first render for data in open
+    if (!this._isOpen) return this.open();
+
+    this._surfaceElement.element.querySelector('mdw-list').innerHTML = this._data.map(d => this._rowTemplate(d)).join('\n');
+
+    this._showSuggestion();
   }
 
-  onPanelClose(e) {
-    const target = this.targetInput;
-    this.panel.removeEventListener('click', this.bound_onPanelClick);
-    target.removeEventListener('change', this.bound_onTargetChange);
+  _showSpinner() {
+    if (this._showSpinner === true) return;
+    this._showSpinner = true;
+    const isWhite = this.classList.contains('mdw-primary') || this.classList.contains('mdw-secondary') || this.classList.contains('mdw-error');
+    this._spinnerContainer.innerHTML = `<mdw-circular-progress mdw-mode="indeterminate" mdw-diameter="24" class="${isWhite ? 'mdw-white' : 'mdw-grey'}" style="position: absolute; right: 6px; top: calc(50% - 12px);"></mdw-circular-progress>`;
   }
 
-  openPanel() {
-    const target = this.targetInput;
-    this._focusIndex = undefined;
-    this.panel.open(true);
-    this.panel.addEventListener('click', this.bound_onPanelClick);
-    target.addEventListener('change', this.bound_onTargetChange);
+  _hideSpinner() {
+    this._showSpinner = false;
+    this._spinnerContainer.innerHTML = '';
   }
 
-  onKeyDown(e) {
-    if (!this.panel.isOpen()) {
-      if (!this._isInputFocused) return;
-      if (e.keyCode !== 27) this.openPanel();
+  _onFocus() {
+    this._canOpen = true;
+  }
+
+  _onBlur() {
+    if (this._mouseIsDown) return;
+
+    this._canOpen = false;
+    this.close();
+  }
+
+  _onInput() {
+    this._showSuggestion();
+  }
+
+  _showSuggestion() {
+    if (this._isSuggestionsEnabled !== true) return;
+
+    const value = this.input.value;
+    if (!value) this._removeSuggestion();
+
+    const match = this._data.find(({ text }) => text.toLowerCase().indexOf(value.toLowerCase()) === 0);
+
+    if (!match) return this._removeSuggestion();
+    this._suggestionHideContainer.innerHTML = value;
+    this._suggestionShowContainer.innerHTML = match.text.replace(value, '');
+  }
+
+  _removeSuggestion() {
+    if (this._isSuggestionsEnabled !== true) return;
+
+    this._suggestionHideContainer.innerHTML = '';
+    this._suggestionShowContainer.innerHTML = '';
+  }
+
+  _mouseDown() {
+    this._mouseIsDown = true;
+  }
+
+  _mouseUp() {
+    this._mouseIsDown = false;
+  }
+
+  _onClick(event) {
+    if (!this.hasAttribute('mdw-index')) {
+      const elementText = event.target.outerHTML;
+      const found = this._data.find(({ text }) => elementText.includes(text));
+      if (!found) throw Error('mdw-autocomplete: could not match item clicked');
+
+      this.input.value = found.text;
+      this.close();
+      this.dispatchEvent(new CustomEvent('change', { detail: found }));
       return;
     }
+
+    const index = event.target.getAttribute('mdw-index');
+    const item = this._data[index];
+    this.input.value = item.text;
+    this.close();
+    this.dispatchEvent(new CustomEvent('change', { detail: item }));
+  }
+
+  _onKeydown(e) {
+    if (!this._isOpen) return;
 
     switch (e.keyCode) {
       case 40: //down
-      case 39: //right
-        this.focusNext();
+        this._focusNext();
         break;
 
       case 38: //up
-      case 37: //left
-        this.focusPrevious();
+        this._focusPrevious();
         break;
 
       case 13: //enter
-        this.selectFocused();
+        this._selectFocused();
+        break;
+
+      case 27: //esc
+        this.close();
         break;
     }
   }
 
-  onTargetFocus(e) {
-    this._isInputFocused = true;
-    this.openPanel();
-  }
+  _focusNext() {
+    if (!this._isOpen) return;
 
-  onTargetBlur(e) {
-    this._isInputFocused = false;
-    this.panel.close();
-    this.panel.removeEventListener('click', this.bound_onPanelClick);
-  }
-
-  onTargetChange(e) {
-    // console.log('change');
-  }
-
-  onTargetInput(e) {
-    if (!this.panel.isOpen()) {
-      this.openPanel();
-      return;
-    }
-    if (this._hasFilter) this.debounce_filter(e.target.value);
-  }
-
-  onPanelClick(e) {
-    if (e.target.hasAttribute('value')) {
-      const value = e.target.getAttribute('value');
-      // TODO text field should do this when value is set
-      this.targetInput.parentNode.classList.add('not-empty');
-      this.targetInput.value = value;
-      this.panel.close();
-    }
-  }
-
-  filter(value) {
-    if (!this.panel.isOpen()) return;
-    value = value.toLowerCase();
-    const vlen = value.length;
-    const filtered = Object.keys(this._optionsData).filter(h => {
-      const hlen = h.length;
-      if (vlen > hlen) return false;
-      if (vlen === hlen) return value === h;
-      if (h.toLowerCase().includes(value)) return true
-      return false;
-      // TODO implement char matching
-      // let i = 0;
-      // let j;
-      // for (; i < vlen; i += 1) {
-      //
-      // }
-    });
-
-    this.panel.innerHTML = this.renderOptions(filtered);
-  }
-
-  renderOptions(optionKeys) {
-    return `${optionKeys.map(k => `
-      <option value="${this._optionsData[k]}">${k}</option>
-    `).join('\n')}`;
-  }
-
-  focusNext() {
-    if (!this.panel.isOpen()) return;
-    const optionElements = [...this.panel.children];
+    const elements = [...this._surfaceElement.element.querySelector('mdw-list').children];
     if (this._focusIndex === undefined) this._focusIndex = 0;
     else this._focusIndex += 1;
-    if (this._focusIndex > optionElements.length - 1) this._focusIndex = optionElements.length - 1;
-    if (this._focusedOption) this._focusedOption.classList.remove('mdw-focused');
-    this._focusedOption = optionElements[this._focusIndex];
+    if (this._focusIndex > elements.length - 1) this._focusIndex = elements.length - 1;
+    this._unFocusItem();
+
+    this._focusedOption = elements[this._focusIndex];
     this._focusedOption.classList.add('mdw-focused');
   }
 
-  focusPrevious() {
-    if (!this.panel.isOpen()) return;
-    const optionElements = [...this.panel.children];
+  _focusPrevious() {
+    if (!this._isOpen) return;
+
+    const elements = [...this._surfaceElement.element.querySelector('mdw-list').children];
     if (this._focusIndex === undefined) this._focusIndex = 0;
     else this._focusIndex -= 1;
     if (this._focusIndex <= 0) this._focusIndex = 0;
-    if (this._focusedOption) this._focusedOption.classList.remove('mdw-focused');
-    this._focusedOption = optionElements[this._focusIndex];
+    this._unFocusItem();
+
+    this._focusedOption = elements[this._focusIndex];
     this._focusedOption.classList.add('mdw-focused');
   }
 
-  selectFocused() {
-    if (!this.panel.isOpen()) return;
-    const optionElements = [...this.panel.children];
-    if (this._focusIndex == undefined || this._focusIndex > optionElements.length - 1) this._focusIndex = 0;
-    const value =  optionElements[this._focusIndex].getAttribute('value');
-    // TODO text field should do this when value is set
-    this.targetInput.parentNode.classList.add('not-empty');
-    this.targetInput.value = value;
-    this.panel.close();
+  _selectFocused() {
+    if (!this._isOpen) return;
+
+    const elements = [...this._surfaceElement.element.querySelector('mdw-list').children];
+
+    if (this._focusIndex == undefined || this._focusIndex > elements.length - 1) this._focusIndex = 0;
+    
+    const value = this._data[this._focusIndex];
+    this.input.value = value.text;
+    this.close();
+
+    this.dispatchEvent(new CustomEvent('change', { detail: value }));
+  }
+
+  _resetFocus() {
+    this._unFocusItem();
+    this._focusIndex = undefined;
+  }
+
+  _unFocusItem() {
+    if (this._focusedOption) this._focusedOption.classList.remove('mdw-focused');
+    this._focusedOption = undefined;
+  }
+
+  _defaultTemplate(data) {
+    return `<mdw-list-item>${data.text}</mdw-list-item>`;
+  }
+
+  _renderRow(data, index) {
+    const row = this._rowTemplate(data);
+
+    if (row.includes('mdw-index=')) return row;
+    if (row.includes('<mdw-list-item')) return row.replace('<mdw-list-item', `<mdw-list-item mdw-index="${index}"`);
+    if (row.indexOf('<div') === 0) return row.trim().replace('<div', `<div mdw-index="${index}"`);
+
+    console.warn('mdw-autocomplete: cannot auto add "mdw-index" attribute to row template, please add mdw-index to guarantee item selection works correctly.');
+    return row;
   }
 });
