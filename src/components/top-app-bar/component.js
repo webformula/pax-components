5c13534c6a76018e8824676d80061cb3964b8d53
Ben Rubin <ben@benjaminrubin.com>
2022-09-21 11:19:28 -0500

    up

--- src/components/top-app-bar/component.js
+++ src/components/top-app-bar/component.js
@@ -2,23 +2,67 @@ import HTMLElementExtended from '../HTMLElementExtended.js';
 import './component.css';
 import util from '../../core/util.js';
 
+
 customElements.define('mdw-top-app-bar', class MDWButton extends HTMLElementExtended {
   useShadowRoot = false;
 
+  #scrollTarget;
+  #lastScrollTop;
+  #currentDirection;
+  #distanceFromDirectionChange;
+  #isHiding = false;
+  #startingScrolled;
   #scrollHandler_bound = util.rafThrottle(this.#scrollHandler).bind(this);
 
   constructor() {
     super();
+    
   }
 
   connectedCallback() {
-    setTimeout(() => {
-      const scrollTarget = document.querySelector('page-content') || document.querySelector('mdw-page-content');
-      if (scrollTarget) scrollTarget.addEventListener('scroll', this.#scrollHandler_bound);
-    }, 0);
+    this.#scrollTarget = document.querySelector('html');
+    this.#lastScrollTop = this.#scrollTarget.scrollTop;
+    document.addEventListener('scroll', this.#scrollHandler_bound);
+    document.addEventListener('touchmove', this.#scrollHandler_bound);
   }
 
-  #scrollHandler(event) {
-    this.classList.toggle('mdw-scrolled', event.target.scrollTop > 0);
+  hide() {
+    if (this.#isHiding === true) return;
+    this.#isHiding = true;
+
+    this.classList.add('mdw-hide');
+  }
+
+  show() {
+    if (this.#isHiding === false) return;
+    this.#isHiding = false;
+
+    this.classList.remove('mdw-hide');
+  }
+
+  #scrollHandler() {
+    // scroll event is fired if the page is refreshed with scroll
+    // if (!this.#lastScrollTop) {
+    //   this.#lastScrollTop = this.#scrollTarget.scrollTop;
+    //   return;
+    // }
+
+    const distance = this.#scrollTarget.scrollTop - this.#lastScrollTop;
+    if (distance === 0) return;
+
+    const direction = this.#scrollTarget.scrollTop >= this.#lastScrollTop ? -1 : 1;
+    if (direction !== this.#currentDirection) this.#distanceFromDirectionChange = 0;
+    this.#currentDirection = direction;
+
+    this.#distanceFromDirectionChange += distance;
+
+    // up
+    if (direction === -1 && this.#distanceFromDirectionChange > 150) this.hide();
+
+    // down
+    if (direction === 1 && this.#distanceFromDirectionChange < -150) this.show();
+
+    this.#lastScrollTop = this.#scrollTarget.scrollTop;
+    this.classList.toggle('mdw-scrolled', this.#scrollTarget.scrollTop > 0);
   }
 });
