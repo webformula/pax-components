495dc7a5859dffe61cde1dbe7445fd175f09e3a1
Ben Rubin <ben@benjaminrubin.com>
2022-10-26 11:39:18 -0500

    up

--- src/components/checkbox/component.js
+++ src/components/checkbox/component.js
@@ -3,7 +3,6 @@ import util from '../../core/util.js'
 import Ripple from '../../core/Ripple.js';
 import styleAsString from '!!css-loader!./component.css?raw';
 
-// TODO indeterminate
 
 customElements.define('mdw-checkbox', class MDWCheckbox extends HTMLElementExtended {
   useShadowRoot = true;
